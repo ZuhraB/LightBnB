a18855faf3ab1fa2fc44dff2e069f96fee1b1da1
ZuhraB <zuhra.bakhshi.com>
2020-09-19 13:54:50 +0000

    added the options for properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -93,11 +93,31 @@ const getAllProperties = function(options, limit = 10) {
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     queryString += `WHERE city LIKE $${queryParams.length} `;
-  } if (options.owner_id) {
-    queryParams.push(`${options.properties.owner_id}`);
-    queryString += `WHERE properties.owner_id LIKE $${queryParams.length}`
-  } if (options.minimum_price_per_night && options.maximum_price_per_night)
+   if (options.owner_id) {
+      queryParams.push(options.owner_id);
+      if(queryParams.length === 1) {
+        queryString += `WHERE owner_id = $${queryParams.length}`
+      } else {
+        queryString += `AND WHERE owner_id = $${queryParams.length}`
+      }
+    }
+  } 
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
+    if (queryParams.length === 2) {
+      queryString += `WHERE cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
+    } else {
+      queryString += `AND cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `;
+    }
+  }
+  queryString += `
+  GROUP BY properties.id
+  `;
 
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
   // 4
   queryParams.push(limit);
   queryString += `
